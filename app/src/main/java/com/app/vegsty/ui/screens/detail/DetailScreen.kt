da8a8fd8c3154e9d6c049f1846584d42dd7ab967
ZaurAlimov06 <alimov.zaur06@gmail.com>
2023-06-04 02:49:11 +0400

    Recipes Detail screen is added

--- app/src/main/java/com/app/vegsty/ui/screens/detail/DetailScreen.kt
+++ app/src/main/java/com/app/vegsty/ui/screens/detail/DetailScreen.kt
@@ -10,8 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.app.vegsty.data.remote.dto.Recipe
+import com.app.vegsty.ui.components.RecipeImage
 import com.app.vegsty.ui.route.NavigationType
+import com.app.vegsty.ui.theme.DarkBackgroundColor
 import com.app.vegsty.ui.theme.Dimension
 import com.app.vegsty.ui.theme.LocalSpacing
 import com.app.vegsty.ui.theme.VegstyTheme
@@ -32,13 +35,126 @@ fun DetailScreen(
   ) {
 
     item {
+      if (recipe != null) {
+        RecipeImage(
+          imageUrl = recipe.imgUrl,
+          onLoadImageAgainClick = {
+
+          }
+        )
+      }
+
+      if (recipe != null) {
+        Text(
+          text = recipe.title,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 16.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
       Text(
-        text = recipe?.title ?: "",
+        text = "Details",
         textAlign = TextAlign.Start,
         modifier = Modifier
-          .fillMaxWidth(),
-        style = MaterialTheme.typography.bodySmall,
+          .fillMaxWidth()
+          .padding(horizontal = 10.dp, vertical = 16.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = DarkBackgroundColor
       )
+      if (recipe != null) {
+        Text(
+          text = recipe.detail,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 10.dp, vertical = 16.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
+      Text(
+        text = "Calorie(Kkal)",
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 5.dp, vertical = 8.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = DarkBackgroundColor
+      )
+      if (recipe != null) {
+        Text(
+          text = recipe.calorie,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp, vertical = 8.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
+      Text(
+        text = "Protein(gram)",
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 5.dp, vertical = 8.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = DarkBackgroundColor
+      )
+      if (recipe != null) {
+        Text(
+          text = recipe.protein,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp, vertical = 5.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
+      Text(
+        text = "Fat(gram)",
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 5.dp, vertical = 8.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = DarkBackgroundColor
+      )
+      if (recipe != null) {
+        Text(
+          text = recipe.fat,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp, vertical = 8.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
+      Text(
+        text = "Carbs(gram)",
+        textAlign = TextAlign.Start,
+        modifier = Modifier
+          .fillMaxWidth()
+          .padding(horizontal = 5.dp, vertical = 8.dp),
+        style = MaterialTheme.typography.bodyLarge,
+        color = DarkBackgroundColor
+      )
+      if (recipe != null) {
+        Text(
+          text = recipe.carbs,
+          textAlign = TextAlign.Start,
+          modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 5.dp, vertical = 8.dp),
+          style = MaterialTheme.typography.bodyLarge,
+          color = DarkBackgroundColor
+        )
+      }
     }
   }
 }
